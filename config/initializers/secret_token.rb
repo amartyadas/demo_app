cf9be56f4a2b409175257421b616f5843446cd25
amartyadas <amartya@actwitty.com>
2012-08-22 15:35:26 +0530

    Amartya_First_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'cd8d11e07b4dd5b24b9ec90ce911dafb06a578dc236287dd46faadeef7ce60753beb70a950033bd96e8b744e7ba4a459d6e6d399c8b805362f63088fe02877c0'
